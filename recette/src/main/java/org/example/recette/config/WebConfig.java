0d6d5d588a8f3c76fbcdd1bc6271a1e1977f3d2a
VI Loic <vaninghelandtloic@gmail.com>
2024-09-17 15:01:11 +0200

    28 - Ajout du fichier de config pour gérer l'erreur CORS

---
+++ recette/src/main/java/org/example/recette/config/WebConfig.java
@@ -0,0 +1,24 @@
+package org.example.recette.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Bean
+    public WebMvcConfigurer corsConfigurer()
+    {
+        String[] allowDomains = new String[2];
+        allowDomains[0] = "http://localhost:4200";
+        allowDomains[1] = "http://localhost:8080";
+
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins(allowDomains);
+            }
+        };
+    }
+}
